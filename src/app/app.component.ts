0f6fcb75d0e2d556ef735fc1ff40a2f0e6fe35f5
Rubén Peregrina Aroca <peregrinaruben@gmail.com>
2023-02-22 12:44:58 +0100

    Create google-map component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'google-maps-angular';
+export class AppComponent implements OnInit {
+  hasPosition = false;
+  position = {
+    lat: 41.25,
+    lng: 1.55
+  };
+
+  label = {
+    color: 'white',
+    text: 'You are here'
+  }
+
+  ngOnInit(): void {
+    this.getUserLocation();
+  }
+
+  getUserLocation() {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(position => {
+        this.position.lat = position.coords.latitude;
+        this.position.lng = position.coords.longitude;
+        this.hasPosition = true;
+      });
+    }
+    else {
+      alert("Geolocation is not supported by this browser.");
+    }
+  }
 }
